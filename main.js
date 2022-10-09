4a1687b45d42e5fd4a581144716b0b5782ac32da
Dani-Nicole Kuusk <dani-nicole.kuusk@voco.ee>
2022-10-09 13:10:31 +0300

    Lesson3

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data: function() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg'
         }
     }
 })
