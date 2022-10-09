e7433d05cea8b4232503c3542ebd05b2078367a6
Dani-Nicole Kuusk <dani-nicole.kuusk@voco.ee>
2022-10-09 22:05:39 +0300

    Lesson8

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id:2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id:2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                { id:2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                { id:2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
             ]
         }
     },
@@ -16,8 +16,19 @@ const app = Vue.createApp({
         addToCart () {
             this.cart += 1
         },
-        updateImage(variantImage)  {
-            this.image = variantImage 
+        updateVariant(index)  {
+            this.selectedVariant = index
+        }
+    }, 
+    computed: {
+        title () {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
